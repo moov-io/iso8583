41151c13799997a686953eb59db9af2b6367d22b
Adam Shannon <adamkshannon@gmail.com>
2020-06-10 18:14:53 -0500

    all: add copyright headers

--- field.go
+++ field.go
@@ -1,3 +1,7 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
 package iso8583
 
 const (
---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package iso8583
+
+// Version number
+const Version = "v0.1.0-dev"
