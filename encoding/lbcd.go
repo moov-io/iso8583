c1eb4378a8a8f95c5207674308b73819cab5062e
Pavel Gabriel <alovak@gmail.com>
2021-01-14 11:33:20 +0100

    add lbcd encoding

---
+++ encoding/lbcd.go
@@ -0,0 +1,35 @@
+package encoding
+
+import (
+	"github.com/yerden/go-util/bcd"
+)
+
+var LBCD Encoder = &lBCDEncoder{}
+
+type lBCDEncoder struct{}
+
+func (e *lBCDEncoder) Encode(src []byte) ([]byte, error) {
+	if len(src)%2 != 0 {
+		src = append(src, []byte("0")...)
+	}
+
+	enc := bcd.NewEncoder(bcd.Standard)
+	dst := make([]byte, bcd.EncodedLen(len(src)))
+	n, err := enc.Encode(dst, src)
+	if err != nil {
+		return nil, err
+	}
+
+	return dst[:n], nil
+}
+
+func (e *lBCDEncoder) Decode(src []byte) ([]byte, error) {
+	dec := bcd.NewDecoder(bcd.Standard)
+	dst := make([]byte, bcd.DecodedLen(len(src)))
+	_, err := dec.Decode(dst, src)
+	if err != nil {
+		return nil, err
+	}
+
+	return dst, nil
+}
---
+++ encoding/lbcd_test.go
@@ -0,0 +1,23 @@
+package encoding
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLBCD(t *testing.T) {
+	t.Run("Decode", func(t *testing.T) {
+		res, err := LBCD.Decode([]byte{0x12, 0x30})
+
+		require.NoError(t, err)
+		require.Equal(t, []byte("1230"), res)
+	})
+
+	t.Run("Encode", func(t *testing.T) {
+		res, err := LBCD.Encode([]byte("123"))
+
+		require.NoError(t, err)
+		require.Equal(t, []byte{0x12, 0x30}, res)
+	})
+}
