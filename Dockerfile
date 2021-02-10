d57aaf246e1b7b2f0e806a29d65ff84fe1542c76
Pavel Gabriel <alovak@gmail.com>
2021-02-10 21:45:29 +0100

    keep the bare minimum of files for the new version

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM golang:1.15-alpine as builder
-WORKDIR /go/src/github.com/moov-io/iso8583
-RUN apk add -U make
-COPY . .
-RUN make build
--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The Moov Authors
-// Use of this source code is governed by an Apache License
-// license that can be found in the LICENSE file.
-
-package iso8583
-
-// Version number
-const Version = "v0.2.3"
