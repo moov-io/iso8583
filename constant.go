605da49fcd87372f24ad354f391fac8c5c1f9a38
Louis Heath <lh16421@bristol.ac.uk>
2021-12-08 13:55:22 +0000

    Extend MTI enum (#153)
    * Add Reversal Request Response to MTI enum
    
    * Add 0180 and 0190
    
    * Add administrative messages
    
    * specify that 0180 is positive
    
    * fix whitespace
    
    Co-authored-by: louis <louis@thoughtmachine.net>

--- constant.go
+++ constant.go
@@ -13,11 +13,17 @@ const (
 	// AuthorizationAdvice is when the point-of-sale device breaks down and you have to sign a voucher
 	AuthorizationAdvice MesssageTypeIndicator = "0120"
 
-	// AuthorizationAdviceRepeat used top repeat	If the advice times out
+	// AuthorizationAdviceRepeat used to repeat if the advice times out
 	AuthorizationAdviceRepeat MesssageTypeIndicator = "0121"
 
 	// IssuerResponseToAuthorizationAdvice is a confirmation of receipt of authorization advice
 	IssuerResponseToAuthorizationAdvice MesssageTypeIndicator = "0130"
+
+	// AuthorizationPositiveAcknowledgement indicates that an Authorization Response was received
+	AuthorizationPositiveAcknowledgement MesssageTypeIndicator = "0180"
+
+	// AuthorizationNegativeAcknowledgement indicates that an Authorization Response or Reversal Response was late or invalid
+	AuthorizationNegativeAcknowledgement MesssageTypeIndicator = "0190"
 
 	// AcquirerFinancialRequest is a request for funds, typically from an ATM or pinned point-of-sale device
 	AcquirerFinancialRequest MesssageTypeIndicator = "0200"
@@ -31,17 +37,20 @@ const (
 	// AcquirerFinancialAdviceRepeat is used if the advice times out
 	AcquirerFinancialAdviceRepeat MesssageTypeIndicator = "0221"
 
-	// IssuerResponseToFinancialAdvice	is a confirmation of receipt of financial advice
+	// IssuerResponseToFinancialAdvice is a confirmation of receipt of financial advice
 	IssuerResponseToFinancialAdvice MesssageTypeIndicator = "0230"
 
 	// BatchUpload is a file update/transfer advice
 	BatchUpload MesssageTypeIndicator = "0320"
 
-	// BatchUploadResponse	is a file update/transfer advice response
+	// BatchUploadResponse is a file update/transfer advice response
 	BatchUploadResponse MesssageTypeIndicator = "0330"
 
-	// AcquirerReversalRequest is used to reverses a transaction
+	// AcquirerReversalRequest is used to reverse a transaction
 	AcquirerReversalRequest MesssageTypeIndicator = "0400"
+
+	// AcquirerReversalResponse is a response to a reversal request
+	AcquirerReversalResponse MesssageTypeIndicator = "0410"
 
 	// AcquirerReversalAdvice
 	AcquirerReversalAdvice MesssageTypeIndicator = "0420"
@@ -51,6 +60,18 @@ const (
 
 	// BatchSettlementResponse is a card acceptor reconciliation request response
 	BatchSettlementResponse MesssageTypeIndicator = "0510"
+
+	// AdministrativeRequest is a message delivering administrative data, often free-form and potentially indicating a failure message
+	AdministrativeRequest MesssageTypeIndicator = "0600"
+
+	// AdministrativeResponse is a response to an administrative request
+	AdministrativeResponse MesssageTypeIndicator = "0610"
+
+	// AdministrativeAdvice is an administrative request with stronger delivery guarantees
+	AdministrativeAdvice MesssageTypeIndicator = "0620"
+
+	// AdministrativeAdviceResponse is a response to an administrative advice
+	AdministrativeAdviceResponse MesssageTypeIndicator = "0630"
 
 	// NetworkManagementRequest is used in hypercom terminals initialize request. Echo test, logon, logoff etc
 	NetworkManagementRequest MesssageTypeIndicator = "0800"
