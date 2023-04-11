c55f697927b80be6e8d0943d60a9ba422cf1116a
ddvk <36803246+ddvk@users.noreply.github.com>
2023-04-11 14:30:05 +0200

    fix typo

--- encoding/encoder.go
+++ encoding/encoder.go
@@ -2,7 +2,7 @@ package encoding
 
 type Encoder interface {
 	// Encode packs the data and returns the length in the encoding's representation.
-	// the lenght is usually the number of bytes, but in bcd the number of encoded digits.
+	// the length is usually the number of bytes, but in bcd the number of encoded digits.
 	Encode([]byte) (packed []byte, length int, err error)
 	// Decode returns data decoded into ASCII (or bytes), how many bytes were read, error
 	Decode([]byte, int) (data []byte, read int, err error)
