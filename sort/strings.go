003409cabdd034062b713fd86ab788587fe829bc
PumpkinSeed <qwer.kocka@gmail.com>
2023-09-02 16:53:41 +0200

    Try to remove panic from sort package

--- sort/strings.go
+++ sort/strings.go
@@ -11,15 +11,18 @@ import (
 
 // StringSlice is a function type used to sort a slice of strings in increasing
 // order. Any errors which arise from sorting the slice will raise a panic.
-type StringSlice func(x []string)
+type StringSlice func(x []string) error
 
 // Strings sorts a slice of strings in increasing order.
-var Strings = sort.Strings
+func Strings(x []string) error {
+	sort.Strings(x)
+	return nil
+}
 
 // StringsByInt sorts a slice of strings according to their integer value.
 // This function panics in the event that an element in the slice cannot be
 // converted to an integer
-func StringsByInt(x []string) {
+func StringsByInt(x []string) error {
 	sort.Slice(x, func(i, j int) bool {
 		valI, err := strconv.Atoi(x[i])
 		if err != nil {
@@ -31,22 +34,27 @@ func StringsByInt(x []string) {
 		}
 		return valI < valJ
 	})
+	return nil
 }
 
 // StringsByHex sorts a slice of strings according to their big-endian Hex value.
 // This function panics in the event that an element in the slice cannot be
 // converted to a Hex slice. Each string representation of a hex value must be
 // of even length.
-func StringsByHex(x []string) {
+func StringsByHex(x []string) error {
+	var outerErr error
 	sort.Slice(x, func(i, j int) bool {
 		valI, err := encoding.ASCIIHexToBytes.Encode([]byte(x[i]))
 		if err != nil {
-			panic(fmt.Sprintf("failed to encode ascii hex %s to bytes : %v", x[i], err))
+			outerErr = fmt.Errorf("failed to encode ascii hex %s to bytes : %v", x[i], err)
+			return false
 		}
 		valJ, err := encoding.ASCIIHexToBytes.Encode([]byte(x[j]))
 		if err != nil {
-			panic(fmt.Sprintf("failed to sort strings by hex: %v", err))
+			outerErr = fmt.Errorf("failed to sort strings by hex: %v", err)
+			return false
 		}
 		return new(big.Int).SetBytes(valI).Int64() < new(big.Int).SetBytes(valJ).Int64()
 	})
+	return outerErr
 }
