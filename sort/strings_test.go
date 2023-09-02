7028551e83f750889965844052b4a088abcdf0b2
PumpkinSeed <qwer.kocka@gmail.com>
2023-09-02 17:00:41 +0200

    Add test

--- sort/strings_test.go
+++ sort/strings_test.go
@@ -23,3 +23,10 @@ func TestSortStringsByHex(t *testing.T) {
 	StringsByHex(x)
 	require.Equal(t, []string{"10", "B0", "ABCD"}, x)
 }
+
+func TestStringsByHexError(t *testing.T) {
+	x := []string{"B0", "Z", "ABCD"}
+	err := StringsByHex(x)
+	require.NotNil(t, err)
+	require.Equal(t, "failed to sort strings by hex: failed to perform hex decoding", err.Error())
+}
